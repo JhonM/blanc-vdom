2f63e072bbe5017205e9acbb97d04be7b8928e17
JhonM <jhonmajoor@gmail.com>
2022-06-05 15:58:44 +0200

    Remove and ignore dist dir

--- dist/types.d.ts
+++
@@ -1,8 +0,0 @@
-export const h: (type: any, props: any, ...children: any[]) => {
-    type: any;
-    props: any;
-    children: any[];
-};
-export const render: (vnode: any) => any;
-
-//# sourceMappingURL=types.d.ts.map
