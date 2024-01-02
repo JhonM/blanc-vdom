5ac7107a2fde4961b7d1a87deaa90adb8cfb4ed9
Jhon Majoor <jhonmajoor@gmail.com>
2024-01-02 16:26:33 +0100

    fix(): update basic example

--- demo/DemoAppView.ts
+++ demo/DemoAppView.ts
@@ -1,7 +1,22 @@
 import { h } from "../src/core";
+import { buttonPlus, buttonMinus } from "./components/button";
+
+function h1() {
+  return h("H1", {}, "Simple Example");
+}
 
 export const DemoAppView = h(
   "div",
   { id: "demo_app_view_id" },
-  "Demo App View, still"
+  ...[
+    h1(),
+    buttonMinus({
+      onclick: () => console.info("Subtract"),
+      text: "Subtract-",
+    }),
+    buttonPlus({
+      onclick: () => console.info("Add"),
+      text: "Add+",
+    }),
+  ]
 );
