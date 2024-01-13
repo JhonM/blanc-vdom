037cba7767a579a90bdcff84d543c066c28a69dd
Jhon Majoor <jhonmajoor@gmail.com>
2024-01-14 00:56:17 +0100

    fix(): add event listener more generally

---
+++ src/helpers/__tests__/add-event.spec.ts
@@ -0,0 +1,27 @@
+/**
+ * @jest-environment jsdom
+ */
+import { attachEvent } from "../add-event";
+
+describe("add event", () => {
+  test("it should attach a onclick event", () => {
+    const target = document.createElement("div");
+    const instanceMock = jest.spyOn(target, "click");
+
+    const event = "onclick";
+    const method = jest.fn();
+    const props = {
+      target,
+      event,
+      method,
+    };
+
+    target.addEventListener = jest.fn();
+
+    attachEvent(props);
+    target.click();
+
+    expect(target.addEventListener).toBeCalledWith("onclick", method, false);
+    expect(instanceMock).toBeCalledTimes(1);
+  });
+});
