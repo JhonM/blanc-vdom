dddd054ecb098f2980ba86ccb27c6aab03e1fa35
Jhon Majoor <jhonmajoor@gmail.com>
2024-01-13 22:27:05 +0100

    refactor(): moved helpers vdom func

--- src/core/__tests__/vdom.spec.ts
+++ src/core/__tests__/vdom.spec.ts
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-import { h, changed, render, setProps, setProp, setBooleanProp } from "../vdom";
+import { h, changed, render } from "../vdom";
 
 describe("h function", () => {
   test("it renders the h function with required arguments", () => {
@@ -87,59 +87,13 @@ describe("Update element", () => {
   });
 });
 
-describe("internal functions", () => {
-  test("changed function", () => {
+describe("changed functions", () => {
+  test("it should have changed", () => {
     const node1 = h("div", { id: "one" }, "Node 1");
     const node2 = h("button", { id: "two" }, "Node 2");
 
     expect(changed(node1, node1)).toEqual(false);
     expect(changed(node1, node2)).toEqual(true);
     expect(changed(render("div"), node1)).toEqual(true);
-  });
-
-  test("setProps", () => {
-    const props = {
-      id: "foo",
-      className: "some-classname",
-      title: "some title",
-    };
-    const anchor = document.createElement("a");
-    setProps(anchor, props);
-
-    expect(anchor.id).toBe("foo");
-    expect(anchor.title).toBe("some title");
-    expect(anchor.className).toBe("some-classname");
-  });
-
-  test("setProp with classname", () => {
-    const target = document.createElement("div");
-    setProp({ target, name: "className", value: "a-classname" });
-    expect(target.className).toBe("a-classname");
-  });
-
-  test("setProp with a data-title", () => {
-    const target = document.createElement("div");
-    setProp({ target, name: "id", value: "data-title" });
-    expect(target.id).toBe("data-title");
-  });
-
-  test("setProp with boolean", () => {
-    const target = document.createElement("div");
-    setProp({ target, name: "boolean", value: "true" });
-    expect(target.attributes.item(0)?.name).toEqual("boolean");
-    expect(target.attributes.item(0)?.value).toEqual("true");
-  });
-
-  test("setBooleanProp on true", () => {
-    const target = document.createElement("div");
-    setBooleanProp({ target, name: "boolean", value: "true" });
-    expect(target.attributes.item(0)?.name).toEqual("boolean");
-    expect(target.attributes.item(0)?.value).toEqual("true");
-  });
-
-  test("setBooleanProp on false", () => {
-    const target = document.createElement("div") as any; // use a better type or different kind of test
-    setBooleanProp({ target, name: "boolean", value: "" });
-    expect(target.boolean).toEqual(false);
   });
 });
--- src/core/vdom.ts
+++ src/core/vdom.ts
@@ -1,8 +1,4 @@
-type SetPropsType = {
-  target: any;
-  name: string;
-  value: any;
-};
+import { setProps } from "../helpers";
 
 export const h = (type: any, props: any, ...children: any[]) => {
   return { type, props: props || {}, children };
@@ -31,42 +27,4 @@ export const changed = (node1: any, node2: any) => {
     (typeof node1 === "string" && node1 !== node2) ||
     node1.type !== node2.type
   );
-};
-
-export const setProps = (target: any, props: any) => {
-  Object.keys(props).forEach((name) => {
-    setProp({ target, name, value: props[name] });
-  });
-};
-
-export const setProp = ({ target, name, value }: SetPropsType) => {
-  if (isCustomProp(name)) {
-    return;
-  } else if (name === "className") {
-    target.setAttribute("class", value);
-  } else if (name === "boolean" || name === "selected") {
-    setBooleanProp({ target, name, value });
-  } else if (name === "onclick") {
-    target.addEventListener("click", (e: any) => value(e));
-  } else if (name === "onchange") {
-    target.addEventListener("change", (e: any) => value(e));
-  } else if (name === "onsubmit") {
-    target.addEventListener("submit", (e: any) => value(e));
-  } else {
-    target.setAttribute(name, value);
-  }
-};
-
-export const setBooleanProp = ({ target, name, value }: SetPropsType) => {
-  if (value) {
-    target.setAttribute(name, value);
-    target[name] = true;
-  } else {
-    target[name] = false;
-  }
-};
-
-// TODO: need to make this work, now its just redundant code
-export const isCustomProp = (name: any) => {
-  return false;
 };
