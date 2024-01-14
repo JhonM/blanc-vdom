6d35ae3bc78104e58ece179e85f2433bcf339da6
Jhon Majoor <jhonmajoor@gmail.com>
2024-01-14 12:49:57 +0100

    feat(): adds onsubmit example

--- demo/DemoAppView.ts
+++ demo/DemoAppView.ts
@@ -1,31 +1,75 @@
 import { h } from "../src/core";
-import { buttonPlus, buttonMinus } from "./components/button";
+import { baseButton } from "./components/button";
 import { DispatchType, Model } from "./state/types";
-import { decrementCountMsg, incrementCountMsg } from "./state/update";
+import {
+  decrementCountMsg,
+  incrementCountMsg,
+  submitFormMsg,
+} from "./state/update";
 
-function h1() {
-  return h("H1", {}, "Simple Example");
+function heading(type: string, text: string, props?: { [key: string]: any }) {
+  return h(type, { ...props }, text);
+}
+
+function input({ ...props }: { [key: string]: any }) {
+  return h("input", { ...props });
+}
+
+function form(dispatch: DispatchType, model: Model) {
+  return h(
+    "form",
+    {
+      onsubmit: (e: Event) => {
+        e.preventDefault();
+        dispatch(submitFormMsg((e.target as HTMLFormElement).formNumber.value));
+      },
+    },
+    ...[
+      input({
+        type: "number",
+        name: "formNumber",
+        value: model.form.value.toString(),
+      }),
+    ],
+
+    baseButton({
+      type: "submit",
+      text: "Submit",
+    })
+  );
+}
+
+function formResult(model: Model) {
+  if (Number(model.form.value) > 0) {
+    return h("span", {}, `Form result: ${model.form.value}`);
+  }
+
+  return h("span", {}, "");
 }
 
 export default function DemoAppView(dispatch: DispatchType, model: Model) {
   return h(
     "div",
     {},
-    h1(),
+    heading("H1", "Examples"),
+    heading("H2", "Counter"),
     h(
       "div",
       { style: "display: flex; justify-content: space-evenly;" },
       ...[
-        buttonMinus({
+        baseButton({
           onclick: () => dispatch(decrementCountMsg(model.count)),
           text: "DECREASE",
         }),
         h("div", {}, model.count.toString()),
-        buttonPlus({
+        baseButton({
           onclick: () => dispatch(incrementCountMsg(model.count)),
           text: "INCREASE",
         }),
       ]
-    )
+    ),
+    heading("H2", "Form"),
+    form(dispatch, model),
+    formResult(model)
   );
 }
